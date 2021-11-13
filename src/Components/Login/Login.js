2b12df3bdcc3f2e49288a3a7d0b6c8b95541cad3
clevin m.wilson <clevinmwilson@gmail.com>
2021-11-13 17:30:45 +0530

    login div background edited

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import { useState,useContext } from 'react';
+import { useState, useContext } from 'react';
 import './Login.css';
-import {useNavigate} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 function Login() {
   const navigate = useNavigate();
   const [email, setEmail] = useState('');
-  const [password,setPassword]=useState('');
- 
+  const [password, setPassword] = useState('');
+
   return (
-    <div>
+    <div className='backgroud-image'>
       <div className="loginParentDiv">
-      <h2 className="header-text" >Login</h2>
-      <br />
+        <h2 className="header-text" >Login</h2>
+        <br />
         <form>
-          <label className="label"  htmlFor="fname">Email</label>
+          <label className="label" htmlFor="fname">Email</label>
           <br />
           <input
             className="input"
@@ -21,11 +21,11 @@ function Login() {
             id="fname"
             value={email}
             name="email"
-            onChange={(e)=>{setEmail(e.target.value)}}
+            onChange={(e) => { setEmail(e.target.value) }}
           />
           <br />
           <br />
-          <label className="label"  htmlFor="lname">Password</label>
+          <label className="label" htmlFor="lname">Password</label>
           <br />
           <input
             className="input"
@@ -33,14 +33,14 @@ function Login() {
             id="lname"
             value={password}
             name="password"
-            onChange={(e)=>{setPassword(e.target.value)}}
+            onChange={(e) => { setPassword(e.target.value) }}
           />
           <br />
           <br />
           <button  >Login</button>
         </form>
         <br />
-        <a className='label' onClick={()=>{
+        <a className='label' onClick={() => {
           navigate('/signup')
         }}>Signup</a>
         <br />
