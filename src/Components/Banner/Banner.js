c6a8df4e6ae292b0ea5f309310a192c81fcd6903
Clevin M.Wilson <clevinmwilson@gmail.com>
2023-02-03 19:40:13 +0530

    banner added

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -1,18 +1,29 @@
-import React from "react";
+import axios from '../../axios';
+import React,{useEffect, useState} from "react";
 import "./Banner.css";
+import { API_KEY } from '../../constants/constants';
+import { imageUrl } from '../../constants/constants';
+
 function Banner() {
+  const[banner,setBanner]=useState({});
+  useEffect(()=>{
+    axios.get(`/trending/all/week?api_key=${API_KEY}&language=en-US`).then((response)=>{
+      console.log(response.data.results[Math.floor(Math.random() * 20)]);
+
+      setBanner(response.data.results[Math.floor(Math.random() * 20)])
+    })
+  },[])
   return (
-    <div className="banner">
+    <div style={{
+      backgroundImage: `url(${banner?imageUrl+banner.backdrop_path:""})`}} className="banner">
       <div className="content">
-        <h1 className="title">Title</h1>
+        <h1 className="title">{banner ? banner.original_title:""}</h1>
         <div className="button-wrap">
           <button className="play">Play</button>
           <button className="mylist">My List</button>
         </div>
         <div className="description">
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s,
+          {banner ? banner.overview:""}
         </div>
       </div>
     </div>
---
+++ src/constants/constants.js
@@ -0,0 +1,3 @@
+export const baseUrl = 'https://api.themoviedb.org/3';
+export const imageUrl = 'https://image.tmdb.org/t/p/original';
+export const API_KEY ="00a55f69b6879e4f888721dcec3ad3c7";
